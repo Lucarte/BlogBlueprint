4b7f801017fe81926882603460d298779931044c
LucArte <96082778+Lucarte@users.noreply.github.com>
2024-09-05 09:36:48 +0200

    updated folder placement

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./blog.js", "./src/**/*.{js,ts,jsx,tsx}"],
+	content: [
+		"./index.html",
+		"./blog.js",
+		"./blog.html",
+		"./src/**/*.{js,ts,jsx,tsx}",
+	],
 	theme: {
 		extend: {},
 	},
